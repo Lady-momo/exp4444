64b245018dce8f8d1cb378c2d0ff99a3800fde2d
Lady-momo <zhang_yumo@tju.edu.cn>
2025-05-21 07:47:48 +0800

    Update unet_parts.py

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -38,8 +38,6 @@ class Down(nn.Module):
         )
 
 
-        )
-
     def forward(self, x):
         x = self.maxpool_conv(x)
         ca_weight = self.ca(x)
@@ -58,9 +56,6 @@ class Up(nn.Module):
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
-
-
-        )
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
